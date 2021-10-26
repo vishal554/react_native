9bddb345bf5a14be78ee693181236b0362e0f5d9
vishal <vishal@yodaplus.com>
2021-10-26 14:03:59 +0530

    added icons

--- components/todoitem.js
+++ components/todoitem.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function TodoItem({ item, pressHandler }){
     return (
-        <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}> {item.title} </Text>
-        </TouchableOpacity>
+        
+        <View style={styles.item}>
+            <TouchableOpacity onPress={() => pressHandler(item.key)}>
+                <AntDesign name="delete" size={20} color="black" />
+            </TouchableOpacity>
+            <Text> {item.title} </Text>
+        </View>
+        
     )
 }
 const styles = StyleSheet.create({
     item:{
+        flexDirection: 'row',
         borderWidth: 1,
         borderRadius: 10,
         borderColor: 'red',
