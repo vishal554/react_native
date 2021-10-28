e5e267fc733349dd6f11783f243e910bb230fa42
vishal <vishal@yodaplus.com>
2021-10-28 15:00:14 +0530

    Finished Feedback app

--- styles/global.js
+++ styles/global.js
@@ -14,5 +14,30 @@ export const globalStyles = StyleSheet.create({
     paragraph:{
         marginVertical: 8,
         lineHeight:20,
+    },
+    input:{
+        borderColor:'#ddd',
+        borderRadius:6,
+        fontSize: 18,
+        borderWidth: 1,
+        padding:10,
+
+    },
+    errorText:{
+        color: 'crimson',
+        fontWeight: 'bold',
+        marginBottom: 10,
+        marginTop: 6, 
+        textAlign: 'center'
     }
 });
+
+export const images = {
+    ratings: {
+        '1': require('../assets/rating-1.png'),
+        '2': require('../assets/rating-2.png'),
+        '3': require('../assets/rating-3.png'),
+        '4': require('../assets/rating-4.png'),
+        '5': require('../assets/rating-5.png'),
+    }
+}
